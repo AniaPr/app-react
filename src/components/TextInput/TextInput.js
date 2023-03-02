1b26950c28be7393ca1c73779ae932f0eae16a6f
Anna Prusińska <anna.prusinska12@gmail.com>
2023-03-03 00:52:54 +0100

    add Cards

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -3,9 +3,11 @@ import styles from './TextInput.module.scss';
 const TextInput = (props) => {
   return (
     <input
-      className={styles.input}
-      placeholder={props.placeholder}
       type='text'
+      className={styles.input}
+      value={props.value}
+      onChange={props.onChange}
+      placeholder={props.placeholder}
     />
   );
 };
